7476853d3094fa8b487e4131471717de83248ea4
vanessaG0906 <vanessagomezpalma@gmail.com>
2025-05-10 18:12:22 -0500

    .

--- proyectohotel/app/Models/Empleado.php
+++ proyectohotel/app/Models/Empleado.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     protected $fillable = ['cedula','nombre','apellido','tiposangre', 'fechanacimiento','hotel_id','rol_id', 'estado_id'];
+    public function hotels(){
+        return $this->belongsTo(Hotel::class);
+    }
+    public function restaurantes(){
+        return $this->belongsTo(Restaurante::class);
+    }
 }
