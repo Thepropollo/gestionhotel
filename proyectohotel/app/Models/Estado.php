57a593c3bf949b90a90ae692dad5f970e3cc663b
vanessaG0906 <vanessagomezpalma@gmail.com>
2025-05-10 18:25:21 -0500

    .

--- proyectohotel/app/Models/Estado.php
+++ proyectohotel/app/Models/Estado.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Estado extends Model
 {
     protected $fillable = ['nombre_estado','descripcion'];
+    public function empleados(){
+        return $this->hasMany(Empleado::class);
+    }
+    public function restaurantes(){
+        return $this->belongsTo(Restaurante::class);
+    }
+    public function salones(){
+        return $this->belongsTo(Hotel::class);
+    }
 }
